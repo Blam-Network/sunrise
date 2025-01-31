946d7dbbcbd688ec2cb7fe65f627ff5cf52a3d8d
Codie Newark <a13x231mcsl@gmail.com>
2025-01-31 19:31:36 +0000

    h3 screenshots

---
+++ migrations/20250119183235_h3_blind_screenshot.sql
@@ -0,0 +1,33 @@
+-- Drop table if it exists
+DROP TABLE IF EXISTS halo3.blind_screenshot;
+
+-- Create table
+CREATE TABLE halo3.blind_screenshot (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    unique_id BIGINT,
+    name VARCHAR(16) NOT NULL,
+    description VARCHAR(128) NOT NULL,
+    author VARCHAR(16) NOT NULL,
+    file_type INTEGER NOT NULL,
+    author_is_xuid_online BOOLEAN NOT NULL,
+    author_id BIGINT NOT NULL,
+    size_in_bytes BIGINT NOT NULL,
+    date TIMESTAMP NOT NULL,
+    length_seconds INTEGER NOT NULL,
+    campaign_id INTEGER NOT NULL,
+    map_id INTEGER NOT NULL,
+    game_engine_type INTEGER NOT NULL,
+    campaign_difficulty INTEGER NOT NULL,
+    hopper_id SMALLINT NOT NULL,
+    game_id BIGINT NOT NULL,
+
+    -- screenshot header
+    game_tick INTEGER NOT NULL,
+    film_tick INTEGER NOT NULL,
+    jpeg_length INTEGER NOT NULL,
+    camera_position FLOAT[3] NOT NULL,
+    pixel_width SMALLINT NOT NULL,
+    pixel_height SMALLINT NOT null,
+
+    CONSTRAINT unique_unique_id_date UNIQUE (unique_id, date, game_id)
+);
