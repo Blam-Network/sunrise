d4ed0308af1afa60b68f01d53eb967e846e8c1c7
Codie Newark <a13x231mcsl@gmail.com>
2025-02-01 00:23:31 +0000

    Improved user endpoint failsafe

--- src/features/common/database.rs
+++ src/features/common/database.rs
@@ -1,8 +1,10 @@
 pub mod migrations;
 
-use sqlx::PgPool;
+use sqlx::{PgPool, Postgres};
 use tokio::sync::OnceCell;
 use std::env;
+use std::error::Error;
+use std::time::Duration;
 use dotenv::dotenv;
 
 // Define a static OnceCell for the connection pool
@@ -18,10 +20,18 @@ async fn initialize_pool() -> Result<PgPool, sqlx::Error> {
         .expect("DATABASE_URL must be set in .env file or environment variables");
 
     // Create and return the connection pool
-    PgPool::connect(&database_url).await
+    sqlx::postgres::PgPoolOptions::new()
+        .acquire_timeout(Duration::from_secs(5))
+        .connect(&database_url).await
 }
 
 // Async function to get the connection pool
 pub async fn get_connection_pool() -> &'static PgPool {
     POOL.get_or_try_init(|| async { initialize_pool().await }).await.expect("Error connecting to database")
+}
+
+
+// Async function to get the connection pool
+pub async fn try_get_connection_pool() -> Result<&'static sqlx::Pool<Postgres>, Box<dyn Error + Send + Sync>> {
+    POOL.get_or_try_init(|| async { initialize_pool().await }).await.map_err(Box::from)
 }
