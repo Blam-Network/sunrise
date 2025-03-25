1a0c4e45b3ec4b3f92f99f35ae0432f15c836e63
Codie Newark <a13x231mcsl@gmail.com>
2025-03-25 19:44:02 +0900

    missed from last commit

--- migrations/20250325102519_h3_carnage_inaddr.sql
+++ migrations/20250325102519_h3_carnage_inaddr.sql
@@ -1,2 +1,11 @@
 ALTER TABLE halo3.carnage_report_machine ADD inaddr text NULL;
 ALTER TABLE halo3.carnage_report_machine ADD inaddr_online text NULL;
+
+-- backfill
+UPDATE halo3.carnage_report_machine
+SET
+    inaddr = host(inet '0.0.0.0' + ('x' || lpad(encode(substring(session_secure_address FROM 1 FOR 4), 'hex'), 8, '0'))::bit(32)::bigint),
+    inaddr_online = host(inet '0.0.0.0' + ('x' || lpad(encode(substring(session_secure_address FROM 5 FOR 4), 'hex'), 8, '0'))::bit(32)::bigint),
+    session_secure_address = substring(session_secure_address FROM 9)
+WHERE length(session_secure_address) = 36;
+RETURNING inaddr, inaddr_online, encode(session_secure_address, 'hex');
